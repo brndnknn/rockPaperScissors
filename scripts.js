cb52435db6a16648d31a0559fae607ae6afb11a0
brandon <brndnknn@gmail.com>
2021-07-14 14:22:56 -0500

    work on reset button, page clears when game is over

--- scripts.js
+++ scripts.js
@@ -2,26 +2,7 @@
 //Brando Keenan
 // 6/24/2021
 
-/* 
 
-Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection 
-- and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-Important note: you want to return the results of this function call, not console.log() them. To test this function console.log the results:
-
-function playRound(playerSelection, computerSelection) {
-  // your code here!
-}
-
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
-Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
-You have not officially learned how to “loop” over code to repeat function calls… 
-if you already know about loops from somewhere else (or if you feel like doing some more learning) feel free to use them. 
-If not, don’t worry! Just call your playRound function 5 times in a row. Loops are covered in the next lesson.
-At this point you should be using console.log() to display the results of each round and the winner at the end.
-Use prompt() to get input from the user. Read the docs here if you need to.*/
 
 // random selector for computer
 function computerPlay(){
@@ -91,6 +72,7 @@ let playerPoints = document.getElementById('playerPoints');
 let computerPoints = document.getElementById('computerPoints');
 
 
+
 // function to translate code for rock paper scissors to text
 function numToText(number){
     let text = '';
@@ -136,15 +118,31 @@ function updateScore(playerChoice){
     computerPoints.textContent = "Computer: " + computerScore;
     
     if (playerScore >= 5){
-        alert("player Wins!");
+        console.log("player Wins!");
+        main.hidden = true;
+        resetBtn.textContent = 'You Win! '
 
     } else if (computerScore >= 5){
-        alert("computer wins!");
+        console.log("computer wins!");
+        main.hidden = true;
 
     }
     
 }
 
+let resetBtn = document.getElementById('resetGame');
+resetBtn.addEventListener('click', resetGame);
+
+function resetGame(){
+    console.log('reset pressed');
+    compLastMove.textContent = '';
+    playerLastMove.textContent = '';
+    playerPoints.textContent = ''; 
+    computerPoints.textContent = '';
+    playerScore = 0;
+    computerScore = 0;
+    main.hidden = false;
+}
 
 let btns = Array.from(document.getElementsByTagName('button'));
 
